852d6b498baf436b871f33875da2cc1834cf97cd
Emery Tanghanwaye <emerytang@gmail.com>
2020-06-10 18:59:31 -0500

    zelcrawler: simple refactors

---
+++ ui/debug/TileCounter.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class TileCounter : Label
+{
+    private World World;
+
+    public override void _Ready()
+    {
+        World = GetNode<World>("/root/Game/World");
+    }
+
+  public override void _PhysicsProcess(float delta)
+  {
+      Text = $"Tiles Generated: {World.Tiles}";
+  }
+}
