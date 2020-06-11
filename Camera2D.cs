88a0ffbc354e66100743d06517637b44db0014ab
Emery Tanghanwaye <emerytang@gmail.com>
2020-06-10 19:44:17 -0500

    zelcrawler: improve camera and add reset button

--- Camera2D.cs
+++ Camera2D.cs
@@ -3,22 +3,40 @@ using System;
 
 public class Camera2D : Godot.Camera2D
 {
-    private Vector2 step = new Vector2(0.10f, 0.10f);
+    [Export] private float Step = 0.2f;
+    [Export] private float Min = .5f;
+    [Export] private float Max = 10f;
+
+    private Vector2 _step, _min, _max = Vector2.Zero;
+    
     public override void _Ready()
     {
+        _step = new Vector2(Step, Step);
+        _min = new Vector2(Min, Min);
+        _max = new Vector2(Max, Max);
     }
-    public override void _UnhandledInput(InputEvent e) {
-                if (e.IsActionReleased("zoom_in"))
-                {
-                    if (Zoom - step > Vector2.Zero)
-                    {
-                        Zoom -= step;
-                    }
-                }
-    
-                if (e.IsActionReleased("zoom_out"))
-                {
-                    Zoom += step;
-                }
+
+    public override void _Input(InputEvent e)
+    {
+        if (e.IsActionReleased("zoom_in"))
+        {
+            if (Zoom - _step >= _min)
+            {
+                Zoom -= _step;
+            }
         }
+
+        if (e.IsActionReleased("zoom_out"))
+        {
+            if (Zoom + _step < _max)
+            {
+                Zoom += _step;
+            }
+        }
+
+        if (e.IsActionPressed("zoom_reset"))
+        {
+            Zoom = Vector2.One;
+        }
+    }
 }
