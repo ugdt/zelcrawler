02a36e16b8115196a36593401e1582b3919431cb
Andrew Bueide <andrew@abueide.com>
2020-06-14 19:21:19 -0600

    Start on chonks

---
+++ world/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Godot.Collections;
+
+namespace nextGame.world
+{
+    public class Tile
+    {
+
+        private String name;
+        private Dictionary properties;
+
+        public Tile(string name, Dictionary properties)
+        {
+            this.name = name;
+            this.properties = properties;
+        }
+
+        public string Name => name;
+
+        public Dictionary Properties => properties;
+    }
+}
