456758ec595fda6a95118d5ccb946992bb11eff9
Emery Tanghanwaye <emerytang@gmail.com>
2020-06-27 10:15:46 -0500

    zelcrawler: player: playercamera: add smoothing to camera zooming

--- Player/PlayerCamera.cs
+++ Player/PlayerCamera.cs
@@ -7,7 +7,10 @@ namespace zelcrawler.player
         private Vector2 _step, _min, _max = Vector2.Zero;
         [Export] private float Max = 15f;
         [Export] private float Min = .5f;
-        [Export] private float Step = 0.2f;
+        [Export] private float Step = 0.5f;
+        
+        private int _zoomSpeed = 5;
+        private Vector2 _cachedZoom = Vector2.Zero;
 
         public override void _Ready()
         {
@@ -16,15 +19,38 @@ namespace zelcrawler.player
             _max = new Vector2(Max, Max);
         }
 
+        public override void _PhysicsProcess(float delta)
+        {
+            if (_cachedZoom != Vector2.Zero)
+            {
+                Vector2 zoomEnding = Zoom + _cachedZoom;
+
+                if (zoomEnding < _min)
+                {
+                    zoomEnding = _min;
+                }
+                else if (zoomEnding > _max)
+                {
+                    zoomEnding = _max;
+                }
+                
+                Zoom = Zoom.LinearInterpolate(zoomEnding, _zoomSpeed * delta);
+
+                _cachedZoom = Vector2.Zero;
+            }
+        }
+
         public override void _Input(InputEvent e)
         {
             if (e.IsActionReleased("zoom_in"))
-                if (Zoom - _step >= _min)
-                    Zoom -= _step;
+            {
+                _cachedZoom -= _step * 2;
+            }
 
             if (e.IsActionReleased("zoom_out"))
-                if (Zoom + _step < _max)
-                    Zoom += _step;
+            {
+                _cachedZoom += _step;
+            }
 
             if (e.IsActionPressed("zoom_reset")) Zoom = Vector2.One;
         }
